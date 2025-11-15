8c2d0f30244484585e72bf415434e33aa244e276
Tony_Shuck <tshuck25@yahoo.com>
2025-11-15 09:21:21 -0800

    Updates from 15NOV breakout room

--- User.py
+++ User.py
@@ -1,1 +1,16 @@
-# your User class goes here
+# # your User class goes here
+
+# class Dog: # this is our class definition/blueprint
+#   pass
+
+# fido = Dog() # this is how we create an instance of our Dog class
+
+class User:
+    def __init__(self, User):
+        self.User = User
+
+Tony = User("Tony") 
+
+print(User.Tony)
+
+# fido = Dog("Fido")
